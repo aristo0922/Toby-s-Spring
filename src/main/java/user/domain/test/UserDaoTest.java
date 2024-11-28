202618d693de0f82f593403a250e74b775f4ede6
aristo <dkfud2121@naver.com>
2024-11-28 16:51:22 +0900

    2-12 get() 테스트 기능을 보완한 addAndGet() 테스트

--- src/main/java/user/domain/test/UserDaoTest.java
+++ src/main/java/user/domain/test/UserDaoTest.java
@@ -14,23 +14,47 @@ public class UserDaoTest {
   @Test
   public void addAndGet() throws SQLException, ClassNotFoundException{
     ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-
     UserDao dao = context.getBean("userDao", UserDao.class);
+
+    User user1 = new User("dkfud2121", "장아령", "springno1");
+    User user2 = new User("Ryan Lee", "라이언", "springno2");
+    User user3 = new User("villains", "빌런즈", "springno3");
 
     dao.deleteAll();
     Assertions.assertEquals(dao.getCount(), 0);
 
-    User user = new User();
-    user.setId("hello12");
-    user.setName("XH");
-    user.setPassword("Villains");
-    dao.add(user);
-    Assertions.assertEquals(dao.getCount(), 1);
+    dao.add(user1);
+    dao.add(user2);
+    Assertions.assertEquals(dao.getCount(), 2);
 
-    User user2 = dao.get(user.getId());
+    User userget1 = dao.get(user1.getId());
+    Assertions.assertEquals(userget1.getName(), userget1.getName());
+    Assertions.assertEquals(userget1.getPassword(), userget1.getPassword());
 
-    Assertions.assertEquals(user2.getName(), user.getName());
-    Assertions.assertEquals(user2.getPassword(), user.getPassword());
+    User userget2 = dao.get(user2.getId());
+    Assertions.assertEquals(userget2.getName(), userget2.getName());
+    Assertions.assertEquals(userget2.getPassword(), userget2.getPassword());
   }
 
+  @Test
+  public void count() throws SQLException, ClassNotFoundException{
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+    UserDao dao = context.getBean("userDao", UserDao.class);
+    User user1 = new User("dkfud2121", "장아령", "springno1");
+    User user2 = new User("Ryan Lee", "라이언", "springno2");
+    User user3 = new User("villains", "빌런즈", "springno3");
+
+    dao.deleteAll();
+    Assertions.assertEquals(dao.getCount(), 0);
+
+    dao.add(user1);
+    Assertions.assertEquals(dao.getCount(),1);
+
+    dao.add(user2);
+    Assertions.assertEquals(dao.getCount(), 2);
+
+    dao.add(user3);
+    Assertions.assertEquals(dao.getCount(), 3);
+  }
 }
