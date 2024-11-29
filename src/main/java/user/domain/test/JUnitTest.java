63ace9cb083cf21c4b13e79f42c08e46fafbf198
aristo <dkfud2121@naver.com>
2024-11-29 16:32:43 +0900

    2-25 개선한 JUnit 테스트 오브젝트 생성에 대한 학습 테스트

--- src/main/java/user/domain/test/JUnitTest.java
+++ src/main/java/user/domain/test/JUnitTest.java
@@ -1,32 +1,36 @@
 package user.domain.test;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class JUnitTest {
 
   static JUnitTest testObject;
+  static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 
   @Test
   public void test1() {
-    assertThat(this, is(not(sameInstance(testObject))));
-    testObject = this;
+    assertThat(testObjects, not(hasItem(this)));
+    testObjects.add(this);
   }
 
   @Test
   public void test2() {
-    assertThat(this, is(not(sameInstance(testObject))));
-    testObject = this;
+    assertThat(testObjects, not(hasItem(this)));
+    testObjects.add(this);
   }
 
   @Test
   public void test3() {
-    assertThat(this, is(not(sameInstance(testObject))));
-    testObject = this;
+    assertThat(testObjects, not(hasItem(this)));
+    testObjects.add(this);
   }
 }
