1d84478a50d49870f8b65dae88a2f41997ba5d5c
aristo <dkfud2121@naver.com>
2024-12-16 20:04:14 +0900

    3.3 JDBC 전략 패턴의 최적화

--- src/main/java/user/domain/UserDao.java
+++ src/main/java/user/domain/UserDao.java
@@ -39,17 +39,18 @@ public class UserDao {
   }
 
   public void add(final User user) throws SQLException {
-    class AddStatement implements StatementStrategy{
-      public PreparedStatement makePreparedStatement(Connection c) throws SQLException{
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-        return ps;
-      }
-    }
-    StatementStrategy strategy = new AddStatement();
-    jdbcContextWithStatementStrategy(strategy);
+    jdbcContextWithStatementStrategy(
+        new StatementStrategy() {
+          @Override
+          public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+            return ps;
+          }
+        }
+    );
   }
 
   public User get(String id) throws ClassNotFoundException, SQLException {
@@ -80,8 +81,14 @@ public class UserDao {
   }
 
   public void deleteAll() throws SQLException {
-    StatementStrategy st = new DeleteAllStatement();
-    jdbcContextWithStatementStrategy(st);
+    jdbcContextWithStatementStrategy(
+        new StatementStrategy() {
+          @Override
+          public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+            return c.prepareStatement("delete from users");
+          }
+        }
+    );
   }
 
   public int getCount() throws SQLException {
