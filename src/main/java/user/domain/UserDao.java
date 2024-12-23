4d707a8d403d336ecbeb52d7644ad41549b0076b
aristo <dkfud2121@naver.com>
2024-12-23 15:16:41 +0900

    3.6.1, 미사용 코드 제거

--- src/main/java/user/domain/UserDao.java
+++ src/main/java/user/domain/UserDao.java
@@ -7,9 +7,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.stereotype.Component;
-import user.domain.strategy.StatementStrategy;
 
 @Component
 public class UserDao {
@@ -22,22 +20,6 @@ public class UserDao {
     this.dataSource = dataSource;
   }
 
-  public void jdbcContextWithStatementStrategy(StatementStrategy strategy) throws SQLException{
-    Connection c = null;
-    PreparedStatement ps = null;
-
-    try{
-      c = this.dataSource.getConnection();
-      ps = strategy.makePreparedStatement(c);
-      ps.executeUpdate();
-      System.out.println("execute update prepareStatement");
-    }catch(SQLException e){
-      throw e;
-    }finally{
-      if (ps !=null){ try { ps.close();} catch (SQLException e){}}
-      if (c !=null){ try {c.close();} catch (SQLException e){}}
-    }
-  }
 
   public void add(final User user) throws SQLException {
     this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
