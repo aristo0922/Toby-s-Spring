9a4186890596d09cbadd04ecb04d1d91ee50e280
aristo <dkfud2121@naver.com>
2024-12-30 15:32:32 +0900

    4.1.3 예외 전환과 예외 포장

--- src/main/java/user/domain/UserDao.java
+++ src/main/java/user/domain/UserDao.java
@@ -1,19 +1,24 @@
 package user.domain;
 
 
+import com.mysql.cj.exceptions.MysqlErrorNumbers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import user.exception.DuplicateUserIdException;
 
 @Component
 public class UserDao {
+
   public void setDataSource(DataSource dataSource) {
     this.jdbcTemplate = new JdbcTemplate(dataSource);
   }
+
   private JdbcTemplate jdbcTemplate;
 
   private RowMapper<User> userMapper = new RowMapper<User>() {
@@ -28,8 +33,20 @@ public class UserDao {
   };
 
 
-  public void add(final User user) throws SQLException {
-    this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
+  public void add(final User user) throws DuplicateUserIdException {
+    try {
+      this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(),
+          user.getName(), user.getPassword());
+    } catch (DataAccessException e) {
+      Throwable cause = e.getCause();
+      if (cause instanceof SQLException) {
+        SQLException sqlException = (SQLException) cause;
+        if (sqlException.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+          throw new DuplicateUserIdException(sqlException); // 예외 전환
+        }
+      }
+        throw new RuntimeException(e); //예외 포장
+    }
   }
 
   public User get(String id) {
@@ -41,7 +58,8 @@ public class UserDao {
     this.jdbcTemplate.update("delete from users");
   }
 
-  public int getCount(){
+
+  public int getCount() {
     return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
   }
 
