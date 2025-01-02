7d1d8dd07fca37661c35354375f6cd16b15e8492
aristo <dkfud2121@naver.com>
2025-01-02 20:26:29 +0900

    5-1 정슈형 상수 값으로 정의한 사용자 레벨

--- src/main/java/user/domain/User.java
+++ src/main/java/user/domain/User.java
@@ -6,6 +6,16 @@ public class User {
   String name;
   String password;
 
+  private static final int BASIC = 1;
+  private static final int SILVER = 2;
+  private static final int GOLD = 3;
+
+  int level;
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
   public User() {
   }
 
