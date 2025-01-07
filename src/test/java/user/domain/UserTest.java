809e8dd9153b7d15f170c33442e3343d92dec85c
aristo <dkfud2121@naver.com>
2025-01-07 10:37:33 +0900

    list 5-29 User Test

--- src/test/java/user/domain/UserTest.java
+++ src/test/java/user/domain/UserTest.java
@@ -1,11 +1,42 @@
 package user.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = {DaoForTest.class})
+//@DirtiesContext
 class UserTest {
 
+  User user;
+
+  @BeforeEach
+  void setUp(){
+    user = new User();
+  }
+
+  @Test
+  void upgradeLevel(){
+    Level[] levels = Level.values();
+    for (Level level: levels){
+      if (level.nextLevel() == null) continue;
+      upgrade(level);
+      Assertions.assertEquals(level.nextLevel(), user.getLevel());
+    }
+  }
+
+  @Test
+  void cannotUpgradeLevel(){
+    Level[] levels = Level.values();
+    for (Level level: levels){
+      if (level.nextLevel() != null) continue;
+      Assertions.assertThrows(IllegalStateException.class, () -> upgrade(level));
+    }
+  }
+
+  private void upgrade(Level level){
+    user.setLevel(level);
+    user.upgradeLevel();
+  }
 }
