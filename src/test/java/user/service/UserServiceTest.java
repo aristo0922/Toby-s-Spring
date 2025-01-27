9641fd2f309111a6df4cafb94419ab142a51c552
aristo <dkfud2121@naver.com>
2025-01-27 19:53:13 +0900

    list 6-12 (Test 를 위한) UserDao 오브젝트

--- src/test/java/user/service/UserServiceTest.java
+++ src/test/java/user/service/UserServiceTest.java
@@ -183,4 +183,48 @@ class UserServiceTest {
   private static class MailException extends Exception {
 
   }
+
+  static class MockUserDao implements UserDao{
+
+    private List<User> users;
+    private List<User> updated = new ArrayList();
+
+    private MockUserDao(List<User> users){
+      this.users = users;
+    }
+
+    public List<User> getUpdated(){
+      return this.updated;
+    }
+
+    @Override
+    public List<User> getAll(){
+      return this.users;
+    }
+
+    @Override
+    public void update(User user) {
+      updated.add(user);
+    }
+
+    @Override
+    public void add(User user) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public User get(String id) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteAll() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getCount() {
+      throw new UnsupportedOperationException();
+    }
+  }
 }
